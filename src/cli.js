af3665adb7524f3733db0fdbc8a777029d04e450
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2019-07-13 15:42:15 -0700

    fix(cli): update-notifier

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,15 @@
 #!/usr/bin/env node
 /* istanbul ignore file */
 import program from 'commander';
+import updateNotifier from 'update-notifier';
+
+import pkg from '../package.json';
 import redisDownload from './redis-download';
 
+updateNotifier({ pkg }).notify();
+
 program
+	.version(pkg.version)
 	.option('--download-dir <downloadDir>', 'Download path')
 	.option('--version <version>', 'Redis version to download, "latest" is by default', 'latest')
 	.parse(process.argv);
